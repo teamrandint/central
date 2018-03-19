df1f76f971a28fc94d32bbccd199cf8b0dfb6005
Jarred Hawkins <jarredhawkins11@gmail.com>
2018-03-18 17:02:39 -0700

    added to build scripts

--- parent/build.sh
+++ parent/build.sh
@@ -40,6 +40,18 @@ docker build \
 --build-arg auditport=44455 \
 -t teamrandint/quoteserver .
 
+cd ../triggerserver
+docker build \
+--build-arg triggeraddr=172.20.0.8 \
+--build-arg triggerport=44456 \
+--build-arg quoteaddr=172.20.0.7 \
+--build-arg quoteport=44459 \
+--build-arg auditaddr=172.20.0.3 \
+--build-arg auditport=44455 \
+--build-arg transaddr=172.20.0.6 \
+--build-arg transport=44458 \
+-t teamrandint/triggerserver .
+
 cd ..
 rm images.tar
 docker save teamrandint/quoteserver teamrandint/transactionserver teamrandint/database teamrandint/webserver teamrandint/auditserver > images.tar
