90b3cd7ca6983984dd7ab12338109c5225b1e275
Brady Schnell <bradyschnell@gmail.com>
2018-03-26 13:00:31 -0700

    work on frontend

--- WebServer/serve/html/js/userActions.js
+++ WebServer/serve/html/js/userActions.js
@@ -9,8 +9,55 @@ $(document).ready(function(){
 	// Check if user has a valid login
 	checkCookie(userName);
 	// Main logic of the program here!
-	$('#usernameText').text(userName)
+	$('#usernameText').text(userName);
 });
+
+// Handles the rendering of the page according to the command selection.
+
+function commandChanged(){
+    var selection = $('#commands').val();
+
+    // Display the corresponding input section on the page.
+    switch (selection) {
+        case "ADD":
+            break
+        case "QUOTE":
+            break
+        case "BUY":
+            break
+        case "COMMIT_BUY":
+            break
+        case "CANCEL_BUY":
+            break
+        case "SELL":
+            break
+        case "COMMIT_SELL":
+            break
+        case "CANCEL_SELL":
+            break
+        case "SET_BUY_AMOUNT":
+            break
+        case "CANCEL_SET_BUY":
+            break
+        case "SET_BUY_TRIGGER":
+            break
+        case "SET_SELL_AMOUNT":
+            break
+        case "SET_SELL_TRIGGER":
+            break
+        case "CANCEL_SET_SELL":
+            break
+        case "DUMPLOG":
+            break
+        case "DISPLAY_SUMMARY":
+            break
+    }
+}
+
+function submitRequest() {
+    var command = $('commands').val();
+
+}
 
 function checkCookie(userName) {
     if (userName === "") {
