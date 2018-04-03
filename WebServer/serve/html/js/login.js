f6986354f7638f788d860aed3c9332b354469f9a
Jarred Hawkins <jarredhawkins11@gmail.com>
2018-04-03 15:23:01 -0700

    Update login.js

--- WebServer/serve/html/js/login.js
+++ WebServer/serve/html/js/login.js
@@ -10,7 +10,7 @@ function loginRequest() {
 	var userName = $('#userName').val();
 	$.ajax({
 		type: 'POST',
-		url: URL + "LOGIN/",
+		url: "LOGIN/",
 		data: {username: userName},
 		success: function(data, status){
 			setCookie("dayTradingUsername", $('#userName').val(), 10)
